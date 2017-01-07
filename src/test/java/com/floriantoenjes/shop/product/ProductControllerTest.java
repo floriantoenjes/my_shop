73152c46b30033a63d831dcd4ae3c882898b33a5
Florian Tönjes <skatepainter@yahoo.de>
2017-01-07 09:34:51 +0100

    Implemented first ProductController test

--- src/test/java/com/floriantoenjes/shop/product/ProductControllerTest.java
+++ src/test/java/com/floriantoenjes/shop/product/ProductControllerTest.java
@@ -1,13 +1,38 @@
 package com.floriantoenjes.shop.product;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class ProductControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ProductController productController;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(productController).build();
+    }
+
+    @Test
+    public void viewProductsTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/product/"))
+                .andExpect(MockMvcResultMatchers.view().name("products"))
+//                .andExpect(MockMvcResultMatchers.model().attributeExists("products"));
+    }
 
 }
