8d40c47a0baa964f958784f2a901e949e428c265
Florian Tönjes <skatepainter@yahoo.de>
2017-01-07 11:12:30 +0100

    Implemented empty cart test

--- src/test/java/com/floriantoenjes/shop/purchase/PurchaseControllerTest.java
+++ src/test/java/com/floriantoenjes/shop/purchase/PurchaseControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,6 +18,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
@@ -70,12 +73,17 @@ public class PurchaseControllerTest {
         ProductPurchase productPurchase1 = new ProductPurchase(product1, 5L);
         List<ProductPurchase> productPurchases = new ArrayList<>(Arrays.asList(productPurchase1));
         int productPurchasesSizeBefore = productPurchases.size();
-                when(purchase.getProductPurchases()).thenReturn(productPurchases);
+        purchase.setProductPurchases(productPurchases);
+        when(purchase.getProductPurchases()).thenReturn(productPurchases);
+        doCallRealMethod().when(purchase).setProductPurchases(new ArrayList<>());
         when(productService.findOne(1L)).thenReturn(product1);
 
         mockMvc.perform(get("/purchase/cart/empty"))
                 .andExpect(redirectedUrl("/purchase/cart"));
-        org.junit.Assert.assertNotEquals(productPurchasesSizeBefore, purchase.getProductPurchases().size());
+
+        when(purchase.getProductPurchases()).thenCallRealMethod();
+        assertNotEquals(productPurchasesSizeBefore,
+                purchase.getProductPurchases().size());
     }
 
 }
