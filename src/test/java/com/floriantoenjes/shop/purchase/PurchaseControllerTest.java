5dfbeaadc7df35c12ce9f2a0a95d4175c2683fd9
Florian Tönjes <skatepainter@yahoo.de>
2017-01-07 10:47:38 +0100

    Started implementing emptyCartTest

--- src/test/java/com/floriantoenjes/shop/purchase/PurchaseControllerTest.java
+++ src/test/java/com/floriantoenjes/shop/purchase/PurchaseControllerTest.java
@@ -1,5 +1,6 @@
 package com.floriantoenjes.shop.purchase;
 
+import com.floriantoenjes.shop.product.Product;
 import com.floriantoenjes.shop.product.ProductService;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,15 +10,17 @@ import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-import static org.junit.Assert.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
@@ -61,8 +64,18 @@ public class PurchaseControllerTest {
     }
 
     @Test
-    public void emptyCart() throws Exception {
+    public void emptyCartTest() throws Exception {
+        Product product1 = new Product("Knife", 3.0, 20L);
+        product1.setId(1L);
+        ProductPurchase productPurchase1 = new ProductPurchase(product1, 5L);
+        List<ProductPurchase> productPurchases = new ArrayList<>(Arrays.asList(productPurchase1));
+        int productPurchasesSizeBefore = productPurchases.size();
+                when(purchase.getProductPurchases()).thenReturn(productPurchases);
+        when(productService.findOne(1L)).thenReturn(product1);
 
+        mockMvc.perform(get("/purchase/cart/empty"))
+                .andExpect(redirectedUrl("/purchase/cart"));
+        org.junit.Assert.assertNotEquals(productPurchasesSizeBefore, purchase.getProductPurchases().size());
     }
 
 }
