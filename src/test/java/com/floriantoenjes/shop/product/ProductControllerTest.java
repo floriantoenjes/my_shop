90a1fe3d29cd1ae07be25c3f2a624fc072592809
Florian Tönjes <skatepainter@yahoo.de>
2017-01-07 09:55:44 +0100

    Implemented second ProductController test

--- src/test/java/com/floriantoenjes/shop/product/ProductControllerTest.java
+++ src/test/java/com/floriantoenjes/shop/product/ProductControllerTest.java
@@ -1,8 +1,12 @@
 package com.floriantoenjes.shop.product;
 
+import com.floriantoenjes.shop.purchase.Purchase;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -12,7 +16,14 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
@@ -20,19 +31,40 @@ public class ProductControllerTest {
 
     private MockMvc mockMvc;
 
-    @Autowired
+    @Mock
+    private ProductService productService;
+
+    @Mock
+    private Purchase purchase;
+
+    @InjectMocks
     private ProductController productController;
 
     @Before
     public void setup() {
+        MockitoAnnotations.initMocks(this);
         mockMvc = MockMvcBuilders.standaloneSetup(productController).build();
     }
 
     @Test
     public void viewProductsTest() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/product/"))
-                .andExpect(MockMvcResultMatchers.view().name("products"))
-//                .andExpect(MockMvcResultMatchers.model().attributeExists("products"));
+        List<Product> products = new ArrayList<>(Arrays.asList(new Product("Fork", 10.0, 3L)));
+        when(productService.findAll()).thenReturn(products);
+
+        mockMvc.perform(get("/product/"))
+                .andExpect(view().name("products"))
+                .andExpect(model().attribute("products", equalTo(products)));
     }
+
+    @Test
+    public void viewProductDetailsTest() throws Exception {
+        Product product = new Product("Spoon", 15.0, 5L);
+        when(productService.findOne(1L)).thenReturn(product);
+
+        mockMvc.perform(get("/product/detail/1"))
+                .andExpect(view().name("detail"))
+                .andExpect(model().attribute("product", equalTo(product)));
+    }
+
 
 }
