b54b1e9f07a91633272bea873e98fc58a56d2f1d
Florian Tönjes <skatepainter@yahoo.de>
2017-01-07 09:23:39 +0100

    Began implementation of tests

---
+++ src/test/java/com/floriantoenjes/shop/product/ProductControllerTest.java
@@ -0,0 +1,13 @@
+package com.floriantoenjes.shop.product;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class ProductControllerTest {
+
+}
