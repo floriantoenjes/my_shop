ad992c2e3ae7e66c195fc78c828bd4701e45b9b3
Florian Tönjes <skatepainter@yahoo.de>
2017-06-06 09:40:14 +0200

    Added a default value for quantity

--- src/main/java/com/floriantoenjes/shop/purchase/PurchaseController.java
+++ src/main/java/com/floriantoenjes/shop/purchase/PurchaseController.java
@@ -26,7 +26,7 @@ public class PurchaseController {
     private ProductService productService;
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String addProduct(@RequestParam("id") Long productId, @RequestParam("quantity") Long quantity,
+    public String addProduct(@RequestParam("id") Long productId, @RequestParam(value = "quantity" , defaultValue = "1") Long quantity,
                              RedirectAttributes redirectAttributes) {
         Product product = productService.findOne(productId);
         boolean alreadyInPurchase = false;
